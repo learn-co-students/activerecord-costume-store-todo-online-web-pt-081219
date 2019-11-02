6f49b9599cb0323a5c7101863ae3c699993978fa
github username <github email address>
2019-11-01 18:01:58 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,21 @@
-# Create your costume_stores migration here
+# # Create your costume_stores migration here
+
+# class ClassName
+    
+#     def change
+#         create_table :costume_stores do |c|
+#             t.string :name
+#             t.string :location
+#             t.integer :costume_inventory
+#             t.integer :number of employees
+#             t.text :whether or not it's still in business
+#             t. :opening time
+#             t. :closing time
+#         end   
+#     end
+
+# end
+
+        
+
+    
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+
+
+
+# class HauntedHouses
+
+#         haunted_houses do |c|
+#         t.name
+#         t.location
+#         t.theme
+#         t.price
+#         t.whether they're family friendly or not
+#         t.opening date
+#         t.closing date
+#         t.long description
+#     end
+    
+# end
+
