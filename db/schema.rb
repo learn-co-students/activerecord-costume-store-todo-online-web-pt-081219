1da33832f17a2ce1529d1e41580ba8274a5526d8
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-20 16:54:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -19,6 +29,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
