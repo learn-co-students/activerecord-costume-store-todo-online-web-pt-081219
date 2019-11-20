4d60f6a0f30208ffa648b081e29d26d747ad23ac
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-20 04:07:32 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
     t.string :name 
     t.float :price
+    t.string :image_url
+    t.string :size
+    t.timestamps null: false 
   end 
  end 
 end 
