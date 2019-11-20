a0c793c16da396234017c56b9de83249505e054e
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-20 16:33:17 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,9 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table costume_stores do |t|
+      t.string :name 
+    end 
+  end 
 end
